884566c104ed96e8c3f5135d8cbebdac967142b0
Cian Gannon <cianmagfhionnain@outlook.com>
2018-11-04 21:41:50 +0000

    Added book api

--- mean-angular6/routes/book.js
+++ mean-angular6/routes/book.js
@@ -1,10 +1,46 @@
-
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Book = require('../models/Book.js');
 
-/* GET home page. */
+/* GET ALL BOOKS */
 router.get('/', function(req, res, next) {
-  res.send('Express RESTful API');
+  Book.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* GET SINGLE BOOK BY ID */
+router.get('/:id', function(req, res, next) {
+  Book.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* SAVE BOOK */
+router.post('/', function(req, res, next) {
+  Book.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE BOOK */
+router.put('/:id', function(req, res, next) {
+  Book.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE BOOK */
+router.delete('/:id', function(req, res, next) {
+  Book.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 module.exports = router;
