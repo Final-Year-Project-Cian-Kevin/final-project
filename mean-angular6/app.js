26957ad184af71173cd118173b88b6ec55c8a31c
Cian Gannon <cianmagfhionnain@outlook.com>
2018-11-04 21:36:34 +0000

    Added mongoose

--- mean-angular6/app.js
+++ mean-angular6/app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mean-angular6', { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
 
 var apiRouter = require('./routes/book');
 
