dc22b072bad439d1b0c8d2acd0a8f5ad49fb25ab
Cian Gannon <cianmagfhionnain@outlook.com>
2018-11-12 18:14:33 +0000

    Added basic angular header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  template: `
+  <p>
+    Header Test
+  </p>
+`,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
