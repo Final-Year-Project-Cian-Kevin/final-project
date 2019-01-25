6c20e26b0c104899c9aa33a7cfe3d3f5e000454d
kbarry <g00339811@gmit.ie>
2019-01-25 22:03:41 +0000

    Setup Passport.js

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ var logger = require('morgan');
 
 var mongoose = require('mongoose');
 
-// connect to mongo database called mean-angular6
-mongoose.connect('mongodb://localhost/mean-angular6')
-  .then(() =>  console.log('connection succesful'))
+var passport = require('passport');
+var config = require('./config/database');
+
+// Create a connection to mean-angular6 mongo database  
+// http://www.fullstackjs.com/book/10/connect-mongoose-bluebird.html
+mongoose.Promise = require('bluebird');// Promisify-ing Mongoose
+
+mongoose.connect(config.database, { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('Connection to database succesful'))
   .catch((err) => console.error(err));
 
-var apiRouter = require('./routes/book');// change book to api
+//mongoose.connect('mongodb://localhost/mean-angular6')
+ // .then(() =>  console.log('connection succesful'))
+ // .catch((err) => console.error(err));
 
+ // variable for API route
+var apiRouter = require('./routes/book');// change book to api
 var app = express();
+
+// Initialize passport
+app.use(passport.initialize());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,7 +39,8 @@ app.use('/book-details/:id', express.static(path.join(__dirname, 'dist/mean-angu
 app.use('/book-create', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/book-edit/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
-// api
+
+// Add API route to endpoint URL
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
