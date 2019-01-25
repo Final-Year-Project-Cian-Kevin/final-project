4051266c865fe62fdee27e74bd4e8d31a0d97349
kbarry <g00339811@gmit.ie>
2019-01-25 22:24:49 +0000

    Using bcrypt on usermodel to encrypt password

--- models/User.js
+++ models/User.js
@@ -1,11 +1,55 @@
 
 var mongoose = require('mongoose');
-// build model
+// https://medium.com/@mridu.sh92/a-quick-guide-for-authentication-using-bcrypt-on-express-nodejs-1d8791bb418f
+// use to hash password 
+var bcrypt = require('bcrypt-nodejs');
+
+// build model of userSchema
+// username must be unique and is required
 var UserSchema = new mongoose.Schema({
-    _id: String,
-    username: String,
-    userpassword: String,
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+  password: {
+        type: String,
+        required: true
+    }
 });
+// define pre hook for document
+UserSchema.pre('save', function (next) {
+    var user = this;
+    // if password new or edited
+    if (this.isModified('password') || this.isNew) {
+        // generate a salt and process data for 10 rounds
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            // generate a hash of password
+            bcrypt.hash(user.password, salt, null, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+// compare password for log in
+UserSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
 
 // Export UserSchema model ad User
 module.exports = mongoose.model('User', UserSchema);
