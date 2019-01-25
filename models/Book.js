1cddec68a2fb2ed2d29b1d09944efcc2e002cc52
kbarry <g00339811@gmit.ie>
2019-01-25 22:31:05 +0000

    Modified Book model to require data

--- models/Book.js
+++ models/Book.js
@@ -1,13 +1,42 @@
 var mongoose = require('mongoose');
 
+// var BookSchema = new mongoose.Schema({
+//   isbn: String,
+//   title: String,
+//   author: String,
+//   description: String,
+//   published_year: String,
+//   publisher: String,
+//   updated_date: { type: Date, default: Date.now },
+// });
 var BookSchema = new mongoose.Schema({
-  isbn: String,
-  title: String,
-  author: String,
-  description: String,
-  published_year: String,
-  publisher: String,
-  updated_date: { type: Date, default: Date.now },
+  isbn: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  published_year: {
+    type: String,
+    required: true
+  },
+  publisher: {
+    type: String,
+    required: true
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now
+  }
 });
-
 module.exports = mongoose.model('Book', BookSchema);
