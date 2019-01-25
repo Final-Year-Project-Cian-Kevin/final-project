a0f8dec842698da33e1c142720f6a5ffcec4eb0f
kbarry <g00339811@gmit.ie>
2019-01-25 23:09:33 +0000

    Created Router for auth and RESTful API

--- routes/book.js
+++ routes/book.js
@@ -1,24 +1,181 @@
+// !!!!!!!!!!!!!!!!!!!!!!!! MUST REFACTOR THIS CLASS AS api.js
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Book = require('../models/Book.js');
 
+
+// Authnitcation imports
+var passport = require('passport');
+var config = require('../config/database');
+require('../config/passport')(passport);
+var jwt = require('jsonwebtoken');
+var User = require("../models/user");
+
+
+/**
+ * Create router to register new user
+ */
+router.post('/signup', function (req, res) {
+  if (!req.body.username || !req.body.password) {
+    res.json({
+      success: false,
+      msg: 'Please enter username and password.'
+    });
+  } else {
+    // create user object
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function (err) {
+      if (err) {
+        return res.json({
+          success: false,
+          msg: 'Username already exists, please choose another.'
+        });
+      }
+      res.json({
+        success: true,
+        msg: 'Successful user account created.'
+      });
+    });
+  }
+});
+
+/**
+ * Create router to login
+ */
+router.post('/signin', function (req, res) {
+  User.findOne({
+    username: req.body.username
+  }, function (err, user) {
+    if (err) throw err;
+
+    // check if valid user
+    if (!user) {
+      res.status(401).send({
+        success: false,
+        msg: 'Log in failed. User not found.'
+      });
+    } else {
+      // check if password correct
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.sign(user.toJSON(), config.secret);
+          // return the information including token as JSON
+          res.json({
+            success: true,
+            token: 'JWT ' + token
+          });
+        } else {
+          res.status(401).send({
+            success: false,
+            msg: 'Incorrect password.'
+          });
+        }
+      });
+    }
+  });
+});
+
+
+/**
+ * Create router to add new book ===================> change to post
+ * User must be authorized
+ */
+router.post('/book', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  // rETRIEVE Token from header
+  var token = getToken(req.headers);
+  if (token) { // check if user is authorised
+    // DEBUG
+    console.log(req.body);
+
+    var newBook = new Book({
+      isbn: req.body.isbn,
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+      published_year: req.body.published_year,
+      publisher: req.body.publisher
+    });
+
+    newBook.save(function (err) {
+      if (err) {
+        return res.json({
+          success: false,
+          msg: 'Book(Post) failed to save.'
+        });
+      }
+      res.json({
+        success: true,
+        msg: 'Successful created new book(Post).'
+      });
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      msg: 'Unauthorized to upload.'
+    });
+  }
+});
+
+/**
+ * Create router to get list of all books ===================> change to post
+ * User must be authorized
+ */
+router.get('/book', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    Book.find(function (err, books) {
+      if (err) return next(err);
+      res.json(books);
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      msg: 'Unauthorized.'
+    });
+  }
+});
+
+/**
+ *  parse authorization token from request headers.
+ */
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 /* Return home hope and GET ALL BOOKS */
 
- router.get('/', function(req, res, next) {
-   Book.find(function (err, products) {
-     if (err) return next(err);
+router.get('/', function (req, res, next) {
+  Book.find(function (err, products) {
+    if (err) return next(err);
     // res.json(products);
     res.send('Recieved from api');
-   });
- });
+  });
+});
 /* GET home page. Test api*/
 //router.get('/', function(req, res, next) {
- // res.sendStatus('Recieved from api');
+// res.sendStatus('Recieved from api');
 //});
 
 /* GET SINGLE BOOK BY ID */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
   Book.findById(req.params.id, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -26,7 +183,7 @@ router.get('/:id', function(req, res, next) {
 });
 
 /* SAVE BOOK */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   Book.create(req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -34,7 +191,7 @@ router.post('/', function(req, res, next) {
 });
 
 /* UPDATE BOOK */
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
   Book.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -42,11 +199,12 @@ router.put('/:id', function(req, res, next) {
 });
 
 /* DELETE BOOK */
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
   Book.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
 });
 
+// export router as module
 module.exports = router;
