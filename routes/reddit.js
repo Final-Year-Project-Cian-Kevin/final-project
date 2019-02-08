9d623ab398bf83719661ac9b8b81cdef652543a2
Cian Gannon <cianmagfhionnain@outlook.com>
2019-02-08 20:48:06 +0000

    Added cron job test to reddit api

--- routes/reddit.js
+++ routes/reddit.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var getJSON = require('get-json');
+//var schedule = require('node-schedule');
+var cron = require('node-cron');
+
+cron.schedule('* * * * *', () => {
+ console.log('running a task every minute');
+});
 
 var url = "https://www.reddit.com/r/ProgrammerHumor/top.json"
 
