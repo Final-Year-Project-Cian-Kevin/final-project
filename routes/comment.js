111286ad8f960b97ccca7282bc2185a694a57154
Cian Gannon <cianmagfhionnain@outlook.com>
2019-02-14 16:32:46 +0000

    Comment API working for getting post ID and profile ID

--- routes/comment.js
+++ routes/comment.js
@@ -8,10 +8,32 @@ Get method for getting all user comments
 Link - /comment/all
 */
 router.get('/all', function(req, res){
-    Comments.find(function (err, posts) {
-        if (err) return next(err);
-        res.json(posts);
-      });
+  Comments.find(function (err, posts) {
+      if (err) return next(err);
+      res.json(posts);
+  });
+});
+
+/* 
+Get method for specific comments on a post using ID
+Link - /comment/all/:id
+*/
+router.get('/all/:id', function (req, res, next) {
+  Comments.find({post_id: req.params.id}, function (err, posts) {
+    if (err) return next(err);
+    res.json(posts);
+  });
+});
+
+/* 
+Get method for specific comments made by a suer using user ID
+Link - /comment/all/:id
+*/
+router.get('/all/:id', function (req, res, next) {
+  Comments.find({profile_id: req.params.id}, function (err, posts) {
+    if (err) return next(err);
+    res.json(posts);
+  });
 });
 
 /* 
@@ -19,10 +41,10 @@ Post method to save a comment a user submits on a post
 Link - /comment/post
 */
 router.post('/post', function (req, res, next) {
-    Comments.create(req.body, function (err, post) {
-      if (err) return next(err);
-      res.json(post);
-    });
+  Comments.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
+});
 
 module.exports = router;
