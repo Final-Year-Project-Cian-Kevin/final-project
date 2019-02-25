19014c66130f6a660af89e72f1526ee695cd6aa0
Cian Gannon <cianmagfhionnain@outlook.com>
2019-02-25 18:23:21 +0000

    New reddit api post job done

--- jobs/RedditDatabase.js
+++ jobs/RedditDatabase.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var RedditPH = require('../models/Reddit/PopularFunny.js');
+var RedditPF = require('../models/Reddit/PopularFunny.js');
 var Reddit = require('../models/Reddit/RedditAll.js');
 var fs = require('fs');
 
@@ -12,7 +12,7 @@ var urls = [urlPH, urlSG];
 exports.pop = function () {
 
   // Delete old data
-  RedditPH.remove({}, function(err,removed) {});
+  RedditPF.remove({}, function(err,removed) {});
 
   for(var i = 0; i < urls.length; i++) {
     var posts = [];
@@ -40,7 +40,15 @@ exports.pop = function () {
             }
           }
 
-          console.log(obj.id);
+          posts.push({
+            id: obj.id,
+            title: obj.title,
+            url: obj.url,
+            thumbnail: thumbnailTemp,
+            selftext: obj.selftext,
+            subreddit: obj.subreddit,
+            score: obj.score
+          });
 
           counter++;
 
@@ -51,32 +59,36 @@ exports.pop = function () {
       }
 
       if(completed_requests == urls.length){
-        console.log("Test" + completed_requests + "\n\n");
-      }
+        console.log('\x1b[32m%s\x1b[0m', 'INFO: Updating Reddit API results');
 
+        posts.sort(function (a, b) {return b.score - a.score;});
+
+        for(var i = 0; i < posts.length; i++) {
+          var newRedditPostPH = new RedditPF({
+            _id: posts[i].id,
+            title: posts[i].title,
+            url: posts[i].url,
+            thumbnail: posts[i].thumbnail,
+            selftext: posts[i].selftext,
+            subreddit: posts[i].subreddit,
+        });
+
+        // Save new data to mongoDB
+        newRedditPostPH.save(function (err) {if (err) {}});
+
+        var newRedditPost = new Reddit({
+          _id: posts[i].id,
+          title: posts[i].title,
+          url: posts[i].url,
+          thumbnail: posts[i].thumbnail,
+          selftext: posts[i].selftext,
+          subreddit: posts[i].subreddit
+        });
+          
+        // Save new data to mongoDB
+        newRedditPost.save(function (err) {if (err) {}});
+        }
+      }
     })
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    console.log('\x1b[32m%s\x1b[0m', 'INFO: Updating Reddit API results');
 };
