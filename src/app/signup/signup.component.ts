9c98a85041f80907df097680c0e8b11397c202fb
kevin barry <g00339811@gmit.ie>
2019-03-11 13:38:10 +0000

    [Register]- Adding password validation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { UserService } from '../services/user.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 
 @Component({
@@ -17,11 +18,19 @@ export class SignupComponent implements OnInit {
   signupData = { username: '',email: '',first_name:'',surname:'', password: '' };
   message = '';
 
-  constructor(private router: Router, private api: UserService) {
+  // Form group variables.
+  minimumPwLength = 8;
+  passwordFormGroup: FormGroup;
+
+  constructor(private router: Router, private api: UserService, private formBuilder: FormBuilder) {
     console.log('DEBUG : SignupComponent: IN constructor');
   }
 
   ngOnInit() {
+    this.passwordFormGroup = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(this.minimumPwLength)]],
+      password2: ['', [Validators.required]]
+    }, {validator: this.passwordMatchValidator});
   }
 
   /**
@@ -36,7 +45,28 @@ export class SignupComponent implements OnInit {
         this.message = err.error.msg;
       });
   }
-  
+  /**
+   * Adapted from https://stackoverflow.com/questions/50728460/password-confirm-angular-material
+   *  
+   */
+  /* Shorthands for form controls (used from within template) */
+  get password() { return this.passwordFormGroup.get('password'); }
+  get password2() { return this.passwordFormGroup.get('password2'); }
+
+  /* Called on each input in either password field */
+  onPasswordInput() {
+    if (this.passwordFormGroup.hasError('passwordMismatch'))
+      this.password2.setErrors([{'passwordMismatch': true}]);
+    else
+      this.password2.setErrors(null);
+  }
+
+  passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+    if (formGroup.get('password').value === formGroup.get('password2').value)
+      return null;
+    else
+      return {passwordMismatch: true};
+  };
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error); // log to console instead
