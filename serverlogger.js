881b960c83efdf4c7d0c00a92fe95e6be864b37c
kevin barry <g00339811@gmit.ie>
2019-03-12 22:09:48 +0000

    [serverlog] - Created server log file to handle logging

---
+++ serverlogger.js
@@ -0,0 +1,33 @@
+/**
+ * serverlogger.js is used to implement a basic server logging system.
+ * 
+*/
+
+// Import the fs library
+var fs = require("fs");
+
+// Make logger exportable so any file can reference it
+var Logger = (exports.Logger = {});
+
+// Create 3 write streams to allow to append info, error and debug logs to different streams.
+var infoStream = fs.createWriteStream("logs/info.txt");
+var errorStream = fs.createWriteStream("logs/error.txt");
+var debugStream = fs.createWriteStream("logs/debug.txt");
+
+// Append info message to log file along with the current date
+Logger.info = function(msg) {
+  var message = new Date().toISOString() + " : " + msg + "\n";
+  infoStream.write(message);
+};
+
+// Append debug message to log file along with the current date
+Logger.debug = function(msg) {
+  var message = new Date().toISOString() + " : " + msg + "\n";
+  debugStream.write(message);
+};
+
+// Append error message to log file along with the current date
+Logger.error = function(msg) {
+  var message = new Date().toISOString() + " : " + msg + "\n";
+  errorStream.write(message);
+};
