f96fdb45cf1f0353656c11c905c5c743796a4cd3
kevin barry <g00339811@gmit.ie>
2019-04-25 21:16:25 +0100

    Docs done for apiservice

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,39 +7,69 @@ const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
-// url to API route
+// url to API route.
 const apiUrl = "/api/books";
 
+/**
+ * ApiService handles function calls to the  API route.
+ *
+ * @export
+ * @class ApiService
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  /**
+   *Creates an instance of ApiService.
+   * @param {HttpClient} http
+   * @memberof ApiService
+   */
   constructor(private http: HttpClient) { }
 
+  /**
+  * Handles http errors.
+  *
+  * @private
+  * @param {HttpErrorResponse} error
+  * @returns an observable with a user-facing error message.
+  * @memberof RedditApiService
+  */
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
-      // console.error('DEBUG HANDLE ERROR:',error.error.message)
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
         `Backend returned code ${error.status}, ` +
         `body was: ${error.error}`);
     }
-    // return an observable with a user-facing error message
+    // Return an observable with a user-facing error message.
     return throwError('Something bad happened; please try again later.');
   };
 
-  // Get data out of response
+  /**
+  * Extract data from response.
+  *
+  * @private
+  * @param {Response} res
+  * @returns the body of the response.
+  * @memberof UserService
+  */
   private extractData(res: Response) {
     let body = res;
     return body || {};
   }
 
-  // Get all books 
+  /**
+   * Get all books.
+   *
+   * @returns {Observable<any>}
+   * @memberof ApiService
+   */
   getBooks(): Observable<any> {
     let httpOptions = {
       headers: new HttpHeaders({ 'Authorization': localStorage.getItem('jwtToken') })
@@ -49,7 +79,13 @@ export class ApiService {
       catchError(this.handleError));
   }
 
-  // Get single book using id of book 
+  /**
+   *  Get single book using id of book .
+   *
+   * @param {string} id - book id.
+   * @returns {Observable<any>}
+   * @memberof ApiService
+   */
   getBook(id: string): Observable<any> {
     const url = `${apiUrl}/${id}`;
     return this.http.get(url, httpOptions).pipe(
@@ -57,29 +93,48 @@ export class ApiService {
       catchError(this.handleError));
   }
 
-  // Add a book
-  postBook(data): Observable<any> {
+  /**
+   * Add a book.
+   *
+   * @param {*} data
+   * @returns {Observable<any>}
+   * @memberof ApiService
+   */
+  postBook(data: any): Observable<any> {
     return this.http.post(apiUrl, data, httpOptions)
       .pipe(
-      catchError(this.handleError)
+        catchError(this.handleError)
       );
   }
 
-  // Update a book using book ID
-  updateBook(id: string, data): Observable<any> {
+  /**
+   * Update a book using book ID.
+   *
+   * @param {string} id - book id.
+   * @param {*} data - book data.
+   * @returns {Observable<any>}
+   * @memberof ApiService
+   */
+  updateBook(id: string, data: any): Observable<any> {
     const url = `${apiUrl}/${id}`;
     return this.http.put(url, data, httpOptions)
       .pipe(
-      catchError(this.handleError)
+        catchError(this.handleError)
       );
   }
 
-  // Delete a book using ID
+  /**
+   * Delete a book using ID.
+   *
+   * @param {string} id -book id.
+   * @returns {Observable<{}>}
+   * @memberof ApiService
+   */
   deleteBook(id: string): Observable<{}> {
     const url = `${apiUrl}/${id}`;
     return this.http.delete(url, httpOptions)
       .pipe(
-      catchError(this.handleError)
+        catchError(this.handleError)
       );
   }
 }
