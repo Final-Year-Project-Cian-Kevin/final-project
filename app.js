a72c4d24c84bf7b8ae1733f981ede0dc43ce7113
kevin barry <g00339811@gmit.ie>
2019-04-25 20:23:51 +0100

    Docs done for app js

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ var passport = require('passport');
 var fs = require("fs");
 var swaggerJSDoc = require('swagger-jsdoc');
 
-// Files used by the server side part of the program
+// Files used by the server side part of the program.
 var config = require('./config/database');
 var redditJob = require('./jobs/RedditDatabase.js');
 
-// Setup express routing
+// Setup express routing.
 var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// swagger definition
+// swagger definition.
 var swaggerDefinition = {
   info: {
     title: 'Node Swagger API',
@@ -30,21 +30,21 @@ var swaggerDefinition = {
   basePath: '/',
 };
 
-// options for the swagger docs
+// Options for the swagger docs.
 var options = {
-  // import swaggerDefinitions
+  // import swaggerDefinitions.
   swaggerDefinition: swaggerDefinition,
-  // path to the API docs
+  // path to the API docs.
   apis: ['./**/routes/*.js','routes.js'],// pass all in array 
   };
 
-// initialize swagger-jsdoc
+// Initialize swagger-jsdoc.
 var swaggerSpec = swaggerJSDoc(options);
 
-// Swagger UI route
+// Swagger UI route.
 app.use(express.static(path.join(__dirname, 'public')));
 
-// serve swagger 
+// Serve swagger .
 app.get('/swagger.json', function(req, res) {   
   res.setHeader('Content-Type', 'application/json');  
   res.send(swaggerSpec); }
@@ -57,31 +57,27 @@ app.use(passport.initialize());
 // http://www.fullstackjs.com/book/10/connect-mongoose-bluebird.html
 mongoose.Promise = require('bluebird');// Promisify-ing Mongoose
 
-// Connecto to the mongodb using settings from the config file
+// Connec to to the mongodb using settings from the config file.
 mongoose.connect(config.database, { promiseLibrary: require('bluebird') })
   .then(() =>  console.log('\x1b[32m%s\x1b[0m', 'INFO: Connection to database succesfull'))
   .catch((err) => console.error(err));
 
-//mongoose.connect('mongodb://localhost/mean-angular6')
- // .then(() =>  console.log('connection succesful'))
- // .catch((err) => console.error(err));
-
- // variable for API route
+// Variable for API route.
 var apiRouter = require('./routes/book'); 
 var apiReddit = require('./routes/reddit');
-var apiRouterUser = require('./routes/user');// change book to api
-var apiRouterProfile = require('./routes/profile');// change book to api
+var apiRouterUser = require('./routes/user');
+var apiRouterProfile = require('./routes/profile');
 var apiRouterAssets = require('./routes/assets');
 var apiRouterComment = require('./routes/comment');
 var apiRouterSavePost = require('./routes/savedPost');
-var apiRouterFollow = require('./routes/follow');// change book to api
+var apiRouterFollow = require('./routes/follow');
 
-// Using mean-angular6 database
+// Using mean-angular6 database.
 app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
 
-//create a cors middleware , that adds the cors origin header ro the response, as well as allowed methods for that origin
+// Create a cors middleware , that adds the cors origin header ro the response, as well as allowed methods for that origin.
 app.use(function(req, res, next) {
-  //set headers to allow cross origin request.
+  // Set headers to allow cross origin request.
   console.log("[SERVER INFO]: Access control");
 
       res.header("Access-Control-Allow-Origin", "*");
@@ -90,7 +86,7 @@ app.use(function(req, res, next) {
       next();
 });
 
-// Routes that are handled by angular
+// Routes that are handled by angular.
 app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use('/index', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/login', express.static(path.join(__dirname, 'dist/mean-angular6')));
@@ -104,7 +100,7 @@ app.use('/about', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/follow/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
 
 
-// Add API route to endpoint URL
+// Add API route to endpoint URL.
 app.use('/api', apiRouter);
 app.use('/api/user', apiRouterUser);
 app.use('/api/profile', apiRouterProfile);
@@ -114,21 +110,21 @@ app.use('/api/comment', apiRouterComment);
 app.use('/api/savedPost', apiRouterSavePost);
 app.use('/api/follow', apiRouterFollow);
 
-// Books route
+// Books route.
 app.use('/books', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/book-details/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/book-create', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/book-edit/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/api/books', apiRouter); // Book api route
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler.
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler.
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in development.
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -138,7 +134,7 @@ app.use(function(err, req, res, next) {
   console.log("Debug : aap.js60."+err.status);
 });
 
-// Cron job that runs every minute to update reddit post data and set reddit api subreddits as users
+// Cron job that runs every minute to update reddit post data and set reddit api subreddits as users.
 cron.schedule('* * * * *', () => {
   redditJob.pop(); // Popular posts
   redditJob.news(); // News posts
