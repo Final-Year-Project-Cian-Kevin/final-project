ec2c04bf19d125b34e54a74686a4f902b6086cbd
kevin barry <g00339811@gmit.ie>
2019-04-25 20:28:45 +0100

    Docs done for user js

--- routes/user.js
+++ routes/user.js
@@ -15,29 +15,29 @@
  *         type: string
  */
 
- /**
- * @swagger
- * definition:
- *   userLogin:
- *     properties:
- *       username:
- *         type: string
- *       password:
- *         type: string
- */
+/**
+* @swagger
+* definition:
+*   userLogin:
+*     properties:
+*       username:
+*         type: string
+*       password:
+*         type: string
+*/
 
- // Imports used
+// Imports used.
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-// Authnitcation imports
+// Authentication imports.
 var passport = require('passport');
 var config = require('../config/database');
 require('../config/passport')(passport);
 var jwt = require('jsonwebtoken');
 
-// Import required models
+// Import required models.
 var User = require("../models/user");
 var Profile = require("../models/profile");
 var Follow = require("../models/follow");
@@ -45,10 +45,7 @@ var Follow = require("../models/follow");
 // Import logger to handle server logging. 
 var logger = require("../config/serverlogger").Logger;
 
-/**
-Post router to register new user
-Link - api/user/signup
-*/
+
 /**
  * @swagger
  * /api/user/signup:
@@ -73,13 +70,13 @@ router.post('/signup', function (req, res) {
   console.log('\x1b[34m%s\x1b[0m', "DEBUG : POST USER signup"); //blue cmd
 
   if (!req.body.username || !req.body.password) {
-    logger.error("A user attempted signup with no username or password");
+    logger.info("[Register] A user attempted signup with no username or password");
     res.json({
       success: false,
       msg: 'Please enter username and password.'
     });
   } else {
-    // create user object
+    // Create user object.
     var newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -93,12 +90,12 @@ router.post('/signup', function (req, res) {
 
         // Error message for generic server error.
         var serverError = 'A server error has occurred please try again';
-        var serverErrorMsg="[Register] : register error occured";
+        var serverErrorMsg = "[Register] : register error occured";
 
         // Confirm if error is due to dupliacte email or username added.
         if (err.errmsg.includes('email')) {
           serverError = "Email already in use please choose another";
-          serverErrorMsg= "[Register] : user attempted register with duplicate username";
+          serverErrorMsg = "[Register] : user attempted register with duplicate username";
         } else if (err.errmsg.includes('username')) {
           serverError = "Username already in use please choose another";
           serverErrorMsg = "[Register] : user attempted register with duplicate username";
@@ -153,34 +150,23 @@ router.post('/signin', function (req, res) {
 
     // check if valid user
     if (!user) {
-      logger.error("[Login] : user unsuccesfully attempted log in with username "+req.body.username);
+      logger.error("[Login] : user unsuccesfully attempted log in with username " + req.body.username);
       res.status(401).send({
         success: false,
         msg: 'Log in failed. User not found.'
       });
-      
+
     } else {
-      // check if password correct
+      // Check if password correct.
       user.comparePassword(req.body.password, function (err, isMatch) {
         if (isMatch && !err) {
-          // if user is found and password is right create a token
-          // user.token = user.generateJWT();
-
-          // var token = jwt.sign(user.toJSON(), config.secret);
-          // return the information including token as JSON
-          // res.json({
-          //   success: true,
-          //   token: 'JWT ' + user.toPrivateUserJson()
-          // });
-          logger.info("[Login] : user "+req.body.username+" has succesfully logged in");
+          logger.info("[Login] : user " + req.body.username + " has succesfully logged in");
           res.json({
             success: true,
-
             token: user.generateJWT()
           });
         } else {
-          logger.error("[Login] : user "+req.body.username+" unsuccesfully attempted log in with invalid password ");
-
+          logger.error("[Login] : user " + req.body.username + " unsuccesfully attempted log in with invalid password ");
           res.status(401).send({
             success: false,
             msg: 'Incorrect password.'
@@ -248,12 +234,12 @@ router.get('/profile/:id', function (req, res, next) {
   User.find({
     username: req.params.id
   }).lean().select('username bio image email first_name surname join_date').exec(function (err, user) {
-    logger.info("[Profile] : user "+ req.params.id+" has requested profile info");
+    logger.info("[Profile] : user " + req.params.id + " has requested profile info");
     res.json(user);
   });
 });
 
- /**
+/**
 Update a users details in the database.
 Link - api/user/update/{id}
 */
@@ -285,18 +271,16 @@ Link - api/user/update/{id}
  *           $ref: '#/definitions/user'
  */
 router.put('/update/:id', function (req, res, next) {
-  logger.info("[Profile update](fail) : user "+ req.params.id +" is attempting to update profile.");
 
   User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
     if (err) {
-      logger.error("[Profile update] :"+err);
-      logger.error("[Profile update](fail) : user "+ req.params.id +" attempted to change email to an invalid address.");
+      logger.error("[Profile update](fail) : user " + req.params.id + " attempted to change email to an invalid address.");
       return res.status(401).send({
         success: false,
         msg: 'Email already exists, please choose another.'
       });
     }
-    logger.info("[Profile update](success) : user "+ req.params.id+" has updated profile info");
+    logger.info("[Profile update](success) : user " + req.params.id + " has updated profile info");
     res.json({
       success: true,
       msg: 'Successful user account edited.'
@@ -335,8 +319,7 @@ router.get('/follow/:id', function (req, res) {
     'username': username
   }, function (err, user) {
     if (!user) {
-      console.log("[DEBUG]: /follow : no user found");
-
+      logger.debug("[Follow] : No user found")
       return res.json({
         'state': false,
         'msg': `No user found with username ${username}`,
@@ -344,40 +327,36 @@ router.get('/follow/:id', function (req, res) {
       })
     } else {
       const user_id = user._id;
-
-      console.log("[DEBUG]: /follow :  user found");
-      console.log("[DEBUG]: /follow :  uid", user_id);
+      logger.debug("[Follow] : User found")
 
       Follow.aggregate([{
-          $match: {
-            "user": mongoose.Types.ObjectId(user_id)
-          }
-        },
-        {
-          $lookup: {
-            "from": "users",
-            "localField": "following",
-            "foreignField": "_id",
-            "as": "userFollowing"
-          }
-        },
-        {
-          $lookup: {
-            "from": "users",
-            "localField": "followers",
-            "foreignField": "_id",
-            "as": "userFollowers"
-          }
-        }, {
-          $project: {
-            "user": 1,
-            "userFollowers": 1,
-            "userFollowing": 1
-          }
+        $match: {
+          "user": mongoose.Types.ObjectId(user_id)
         }
+      },
+      {
+        $lookup: {
+          "from": "users",
+          "localField": "following",
+          "foreignField": "_id",
+          "as": "userFollowing"
+        }
+      },
+      {
+        $lookup: {
+          "from": "users",
+          "localField": "followers",
+          "foreignField": "_id",
+          "as": "userFollowers"
+        }
+      }, {
+        $project: {
+          "user": 1,
+          "userFollowers": 1,
+          "userFollowing": 1
+        }
+      }
       ]).exec(function (err, doc) {
-        console.log("[DEBUG] Follow/:id");
-        console.log(doc);
         res.json({
           'state': true,
           'msg': 'Follow list',
