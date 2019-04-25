8ceb353fa1cf2b53567d45a0676fd30d81192091
kevin barry <g00339811@gmit.ie>
2019-04-25 21:43:21 +0100

    Docs done for settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { UserService } from '../services/user.service';
-import { BrowserModule, Title }  from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
-// Import plugin for file upload
+// Import plugin for file upload.
 import { FileUploader } from 'ng2-file-upload/ng2-file-upload';
 
+// API route url.
 const URL = 'http://localhost:3000/api/assets/';
 
+/**
+ * Component for settings page.
+ *
+ * @export
+ * @class SettingsComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -17,9 +25,8 @@ const URL = 'http://localhost:3000/api/assets/';
 export class SettingsComponent implements OnInit {
   profileinfo: any;
   settingsForm: FormGroup;
-  currentUser: any; // user object of current logged in user
-  currentUserName: String; // username of current logged in user
-  //email: string = '';
+  currentUser: any; // user object of current logged in user.
+  currentUserName: String; // username of current logged in user.
   first_name: string = '';
   surname: string = '';
   bio: string = '';
@@ -29,28 +36,48 @@ export class SettingsComponent implements OnInit {
   ]);
   errorMessage = '';
 
-  //declare a property called fileuploader and assign it to an instance of a new fileUploader.
-  //pass in the Url to be uploaded to, and pass the itemAlais, which would be the name of the //file input when sending the post request.
-  public uploader: FileUploader = new FileUploader({ 
-    url: URL, 
+  /**
+   * Declare a property called fileuploader and assign it to an instance of a new fileUploader.
+   * Pass in the Url to be uploaded to, and pass the itemAlais, which would be the name of the file input when sending the post request.
+   * @type {FileUploader}
+   * @memberof SettingsComponent
+   */
+  public uploader: FileUploader = new FileUploader({
+    url: URL,
     itemAlias: 'photo',
     allowedFileType: ['image']
-   });
+  });
 
+  /**
+   *Creates an instance of SettingsComponent.
+   * @param {UserService} userService
+   * @param {Router} router
+   * @param {FormBuilder} formBuilder
+   * @param {Title} titleService
+   * @memberof SettingsComponent
+   */
   constructor(public userService: UserService, private router: Router, private formBuilder: FormBuilder, private titleService: Title) { }
 
-  // Function used to set page title
+  /**
+   *  Function used to set page title
+   *
+   * @param {string} newTitle - new title.
+   * @memberof SettingsComponent
+   */
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
 
-  // Runs on page call
+  /**
+   * Runs on page call.
+   *
+   * @memberof SettingsComponent
+   */
   ngOnInit() {
     this.currentUser = this.userService.getUserPayLoad();
 
     this.currentUserName = this.currentUser.username;
 
-    // this.setForm(this.userService.currentUser.username);
     this.setForm(this.currentUserName);
     this.settingsForm = this.formBuilder.group({
       'email': [null, Validators.email],
@@ -62,11 +89,10 @@ export class SettingsComponent implements OnInit {
     //override the onAfterAddingfile property of the uploader so it doesn't authenticate with //credentials.
     this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
 
-    //overide the onCompleteItem property of the uploader so we are 
-    //able to deal with the server response.
+    // Overide the onCompleteItem property of the uploader so we are able to deal with the server response.
     this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
-     
-      // update the user profile with the new image url
+
+      // Update the user profile with the new image url.
       this.userService.updateUser(this.profileinfo._id, response)
         .subscribe(res => {
           this.router.navigate(['/profile', this.profileinfo.username]);
@@ -76,31 +102,37 @@ export class SettingsComponent implements OnInit {
         );
     };
 
-    // Set page title
+    // Set page title.
     this.setTitle("TB: Profile Settings");
   }
 
-/**
- * Set the form data in SettingsForm to the details of the current user.
- * 
- * @param id The current users id.
- */
-setForm(id) {
-  this.userService.getProfile(id)
-    .subscribe(profile => {
-      // Must extract profile data from response.
-      this.profileinfo = profile[0];
-      // Form object
-      this.settingsForm.setValue({
-        email: this.profileinfo.email,
-        first_name: this.profileinfo.first_name,
-        surname: this.profileinfo.surname,
-        bio: this.profileinfo.bio
+  /**
+   * Set the form data in SettingsForm to the details of the current user.
+   *
+   * @param {*} id - the current users id.
+   * @memberof SettingsComponent
+   */
+  setForm(id: any) {
+    this.userService.getProfile(id)
+      .subscribe(profile => {
+        // Must extract profile data from response.
+        this.profileinfo = profile[0];
+        // Form object
+        this.settingsForm.setValue({
+          email: this.profileinfo.email,
+          first_name: this.profileinfo.first_name,
+          surname: this.profileinfo.surname,
+          bio: this.profileinfo.bio
+        });
       });
-    });
-}
+  }
 
-  // Sends a request to userService to update user details.
+  /**
+   * Sends a request to userService to update user details.
+   *
+   * @param {NgForm} form - the form to submit.
+   * @memberof SettingsComponent
+   */
   onFormSubmit(form: NgForm) {
     this.userService.updateUser(this.profileinfo._id, form)
       .subscribe(res => {
