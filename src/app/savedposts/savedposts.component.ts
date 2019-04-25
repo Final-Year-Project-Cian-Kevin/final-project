a85c681830c295fb39c78b865058b70fa095407c
kevin barry <g00339811@gmit.ie>
2019-04-25 21:49:18 +0100

    Docs done for saved posts

--- src/app/savedposts/savedposts.component.ts
+++ src/app/savedposts/savedposts.component.ts
@@ -6,6 +6,13 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
 
+/**
+ * Component for saved posts.
+ *
+ * @export
+ * @class SavedpostsComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-savedposts',
   templateUrl: './savedposts.component.html',
@@ -17,22 +24,40 @@ export class SavedpostsComponent implements OnInit {
   username;
   id
 
+  /**
+   *Creates an instance of SavedpostsComponent.
+   * @param {ActivatedRoute} route
+   * @param {RedditApiService} api
+   * @param {Router} router
+   * @param {UserService} userAPI
+   * @param {Title} titleService
+   * @memberof SavedpostsComponent
+   */
   constructor(private route: ActivatedRoute, private api: RedditApiService, private router: Router, private userAPI: UserService, private titleService: Title) { }
 
-  // Function to change title of page
+  /**
+   * Function to change title of page.
+   *
+   * @param {string} newTitle - title to set.
+   * @memberof SavedpostsComponent
+   */
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
 
-  // Function runs when page is called
+  /**
+   * Function runs when page is called.
+   *
+   * @memberof SavedpostsComponent
+   */
   ngOnInit() {
-    // Get saved posts using page ID (Username)
+    // Get saved posts using page ID (Username).
     this.getSavedPosts(this.route.snapshot.params['id']);
 
-    // Get page ID
+    // Get page ID.
     this.id = this.route.snapshot.params['id'];
 
-    // Check if user is logged in
+    // Check if user is logged in.
     if (this.userAPI.isLoggedIn()) {
       this.userAPI.getUserData()
         .subscribe(res => {
@@ -45,39 +70,59 @@ export class SavedpostsComponent implements OnInit {
         });
     }
 
-    // Set page title
+    // Set page title.
     this.setTitle("TB: " + this.route.snapshot.params['id'] + "'s Saved Posts");
   }
 
-  // Get saved posts using username
-  getSavedPosts(id) {
+  /**
+   * Get saved posts using username.
+   *
+   * @param {*} id - user id.
+   * @memberof SavedpostsComponent
+   */
+  getSavedPosts(id: any) {
     this.api.getSaved(id)
       .subscribe(data => {
         this.getIDs(data);
       });
   }
 
-  // Get posts using the saved posts ID gotten from the saved posts API
-  getIDs(data) {
+  /**
+   * Get posts using the saved posts ID gotten from the saved posts API.
+   *
+   * @param {*} data - the saved post.
+   * @memberof SavedpostsComponent
+   */
+  getIDs(data: any) {
     for (var i = 0; i < data.length; i++) {
       this.getPostDetails(data[i].post_id);
     }
   }
 
-  // Gets posts details
-  getPostDetails(id) {
+  /**
+   * Get post details.
+   *
+   * @param {*} id - id of post.
+   * @memberof SavedpostsComponent
+   */
+  getPostDetails(id: any) {
     this.api.getPost(id)
       .subscribe(data => {
         this.posts.push(data); // Add post to the posts object
       });
   }
 
-  // Unsub user from the post
-  unsub(id) {
-    // Unsub the user from the post using the post ID and username
+  /**
+   * Unsubscrive user from the post.
+   *
+   * @param {*} id - the  user id.
+   * @memberof SavedpostsComponent
+   */
+  unsub(id: any) {
+    // Unsub the user from the post using the post ID and username.
     this.api.delUnSub(id, this.username)
       .subscribe(res => {
-        location.reload(true); // Page refresh
+        location.reload(true); // Page refresh.
       }, (err) => {
         console.log(err);
       }
