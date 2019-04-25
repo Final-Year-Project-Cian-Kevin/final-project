a8568a290af50368921a41fd908b80afed267b1a
kevin barry <g00339811@gmit.ie>
2019-04-25 21:38:51 +0100

    Docs done for image

--- src/app/shared/image-preview.directive.ts
+++ src/app/shared/image-preview.directive.ts
@@ -1,21 +1,36 @@
-// code adapted from https://github.com/valor-software/ng2-file-upload/issues/461
 import { Directive, ElementRef, Input, Renderer, OnChanges, SimpleChanges } from '@angular/core';
 
+/**
+ * ImagePreview Directive works alongside n2-file-upload to allow an image to be previewed after it is selected for file upload.
+ * Adapted from {@link  https://github.com/valor-software/ng2-file-upload/issues/461|github}.
+ *
+ * @export
+ * @class ImagePreviewDirective
+ */
 @Directive({
   selector: 'img[imgPreview]'
 })
 
-/**
- * ImagePreviewDirective works alongside n2-file-upload to allow an image to be previewed after it is selected for file upload.
- */
 export class ImagePreviewDirective {
 
   // The image to preview.
   @Input() image: any;
 
+  /**
+   *Creates an instance of ImagePreviewDirective.
+   * @param {ElementRef} el
+   * @param {Renderer} renderer
+   * @memberof ImagePreviewDirective
+   */
   constructor(private el: ElementRef, private renderer: Renderer) { }
 
-  // Activated when a change is detected.
+  /**
+   * Activated when a change is detected.
+   *
+   * @param {SimpleChanges} changes
+   * @returns
+   * @memberof ImagePreviewDirective
+   */
   ngOnChanges(changes: SimpleChanges) {
     let reader = new FileReader();
     let el = this.el;
